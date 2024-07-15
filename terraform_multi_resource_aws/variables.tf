1c091fc6c40f0eef560bcccf344990dcbc80f4fc
omarparlak <omparlak@gmail.com>
2024-07-15 16:51:54 -0400

    Update variables.tf

--- terraform_multi_resource_aws/variables.tf
+++ terraform_multi_resource_aws/variables.tf
@@ -1,16 +1,19 @@
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_id" {
   description = "The VPC ID where resources will be created"
   type        = string
+  default     = "vpc-0b7d3c84f12cc1ce6"
 }
 
 variable "security_group_name" {
   description = "Name of the security group"
   type        = string
+  default     = "Terraform Test oparlak"
 }
 
 variable "ssh_port" {
@@ -27,21 +30,25 @@ variable "allowed_cidrs" {
 variable "key_name" {
   description = "Name of the AWS key pair"
   type        = string
+  default     = "id_rsa"
 }
 
 variable "public_key_path" {
   description = "Path to the public key to be used for the key pair"
   type        = string
+  default     = "./id_rsa.pub"
 }
 
 variable "subnet_cidr" {
   description = "CIDR block for the subnet"
   type        = string
+  default     = "10.133.1.0/24"
 }
 
 variable "availability_zone" {
   description = "Availability Zone to create the subnet in"
   type        = string
+  default     = "ust-east-1a"
 }
 
 variable "map_public_ip_on_launch" {
@@ -52,11 +59,14 @@ variable "map_public_ip_on_launch" {
 variable "ami_id" {
   description = "AMI ID of the EC2 instance"
   type        = string
+  default     = "ami-0b72821e2f351e396"
+
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
+  default     = "t2.micro"
 }
 
 variable "associate_public_ip" {
